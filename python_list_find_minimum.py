f1311f662e1dbc756213193c79192c538646bdc2
Zaid-H <54617574+Zaid-H@users.noreply.github.com>
2019-12-15 20:44:13 +0400

    Create python_list_find_minimum.py

---
+++ python_list_find_minimum.py
@@ -0,0 +1,29 @@
+# import the randint function from the random module
+from random import randint
+
+# create a function to generate a list of "size" random integers
+# up to a maximum of "largest"
+
+def random_list(largest,size):
+  # create an empty list
+  l = []
+  # add a random number to the list the appropriate number of times
+  for i in range(size):
+    n = randint(0,largest-1)
+    l.append(n)
+  #print the list to check
+  return(l)
+#call the function
+l = random_list(10,10)
+
+print(l)
+
+def minimum(l):
+  x = 10
+  for i in l:
+    if i < x:
+      x = i
+  return x 
+
+x = minimum(l)
+print(x)      
